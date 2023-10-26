8cf24b967a7af23fc825d2c261f910a79ce9d2e4
AnandaCampelo <anandag.c@hotmail.com>
2023-10-26 14:01:39 -0300

    Tentei

--- client/client.ino
+++ client/client.ino
@@ -1,6 +1,6 @@
 int txPin = 2; // Pino de transmissão (TX)
 int baudRate = 9600; // Velocidade de transmissão
-int rodar = 0;
+int paridade;
 
 void setup() {
   pinMode(txPin, OUTPUT);
@@ -8,40 +8,29 @@ void setup() {
 }
 
 void loop() {
-  if(rodar < 10){
-    char characterToSend = 'A'; // Caractere a ser transmitido
-    sendUARTChar(characterToSend);
-    Serial.println("Bit enviado");
-    delay(1000); // Espera 1 segundo antes de enviar o próximo caractere
-  } else {
-    return 0;
+  Serial.print("Transmição Iniciada!");
+  digitalWrite(txPin, HIGH);
+  myClock(1666);
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(txPin, characterToSend >> i & 1);
+    if ((characterToSend >> i & 1) == 1) {
+      paridade += 1;
+    }
+    myClock(1666);
   }
+  if (paridade % 2 == 0) {
+    Serial.println("Par");
+    digitalWrite(txPin, 0);
+  } else {
+    Serial.println("Impar");
+    digitalWrite(txPin, 1);
+  }
+  Serial.print("Caractere enviado com sucesso!");
+  myClock(1666666);
 }
 
-// character >> i = character[i]
-
-void sendUARTChar(char character) {
-  int bit_paridade = 0;
-  digitalWrite(txPin, LOW); // start bit (LOW)
-  delayMicroseconds(1000000 / baudRate); // Aguarda o tempo de início
-  for (int i = 0; i < 8; i++) {
-    if ((character >> i & 1) == 1) {
-      digitalWrite(txPin, HIGH);
-      bit_paridade ++;
-    } else {
-      digitalWrite(txPin, LOW);
-    }
-    delayMicroseconds(1000000 / baudRate); // Aguarda o tempo de cada bit
-  }
-
-  if(bit_paridade/2 == 0){
-    digitalWrite(txPin, LOW);
-  } else {
-    digitalWrite(txPin, HIGH);
-  }
-
-  digitalWrite(txPin, HIGH); // stop bit (HIGH)
-  delayMicroseconds(1000000 / baudRate); // Aguarda o tempo de parada
-
-  rodar++;
+void myClock(int clock) {
+  for (int i = 0; i < clock*1000000 ;i++){
+      asm("NOP");
+    }  
 }
--- servidor/servidor.ino
+++ servidor/servidor.ino
@@ -1,6 +1,9 @@
 int rxPin = 3; // Pino de recepção (RX)
 int baudRate = 9600; // Velocidade de transmissão
-int rodar = 1;
+int paridade;
+int checkParidade;
+char recebido;
+bool comecou == false;
 
 void setup() {
   pinMode(rxPin, INPUT);
@@ -8,41 +11,38 @@ void setup() {
 }
 
 void loop() {
-  if(rodar == 1){
-    char receivedCharacter = receiveUARTChar();
-    if (receivedCharacter != 0) {
-      Serial.print("Caractere recebido: ");
-      Serial.println(receivedCharacter);
-    }
-  } else {
-    return 0;
+  Serial.print("Servidor Iniciado!");
+  while (!comecou) {
+    comecou = digitalRead(rxPin);
   }
+  Serial.println("Recebendo Transmição...");
+  myClock(2500);
+  for (int i = 0; i < 8; i++) {
+    recebido |= digitalRead(rxPin) << 1;
+    myClock(1666);
+  }
+  paridade = digitalRead(rxPin);
+  for (int i = 0; i < 8; i++) {
+    if ((recebido & (i << 1)) != 0) {
+      checkParidade += 1;
+    }
+  }
+  Serial.print("Bit de Paridade é: ");
+  Serial.print(paridade);
+  Serial.print("Quantidade de 1 foi: ");
+  Serial.print(checkParidade);
 
+  if ((checkParidade % 2 != 0 & paridade == 1) || (checkParidade % 2 == 0 & paridade == 0)) {
+    Serial.println("Transmissão Concluida!");
+  } else {
+    Serial.println("ERRO!");
+  }
+  Serial.println(recebido);
+  myClock(1666666);
 }
 
-char receiveUARTChar() {
-  int bit_paridade = 0;
-  // recebendo start bit
-  int start_bit = digitalRead(rxPin);
-  Serial.print(start_bit);
-  int receivedCharacter = 0;
-  delayMicroseconds(1000000 / baudRate / 2); // Meio tempo de início
-  if(start_bit == LOW){
-    for (int i = 0; i < 8; i++) {
-      if(digitalRead(rxPin) == 1){
-        bit_paridade ++;
-      }
-      int receivedCharacter = ((digitalRead(rxPin) << i) & 1);
-      delayMicroseconds(1000000 / baudRate); // Aguarda o tempo de cada bit
+void myClock(int clock) {
+  for (int i = 0; i < clock*1000000 ;i++){
+      asm("NOP");
     }
-    int bit_paridade_receb = (digitalRead(rxPin));
-    delayMicroseconds(1000000 / baudRate / 2); // Meio tempo de parada
-
-    if(bit_paridade/2 == 0){
-      return receivedCharacter;
-    } else {
-      return 0; // indica que houve erro de paridade
-    }
-  }
-  rodar = 0;
 }
